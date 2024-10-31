47c1cc17bff099b9a443dfd6345141bacc88714e
dilrubamajumder <dilrubamajumder@pursuit.org>
2024-10-30 20:27:19 -0400

    deleting unwanted files

--- do_it-frontend/src/Pages/NewTaskPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ToDoInput from '../Components/ToDoInput'
-export default function NewTaskPage() {
-  return (
-    <div className='new-task-page'>
-        <ToDoInput />
-    </div>
-  )
-}
